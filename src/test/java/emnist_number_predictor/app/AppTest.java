7cc00ad39d0b9fba9ea440074ab2947312ad621d
Seth Gately <sethcg@hotmail.com>
2023-12-25 14:26:00 -0500

    Added unit testing for App file

--- src/test/java/emnist_number_predictor/app/AppTest.java
+++ src/test/java/emnist_number_predictor/app/AppTest.java
@@ -1,35 +1,69 @@
 package emnist_number_predictor.app;
 
+import emnist_number_predictor.components.window.Window;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
 
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+@ExtendWith(ApplicationExtension.class)
 public class AppTest {
-    @Test
-    void testClose() {
 
-    }
+    private App app;
 
-    @Test
-    void testInit() {
-
-    }
-
-    @Test
-    void testMain() {
-
+    @Start
+    private void start(Stage stage) {
+        // Arrange
+        app = new App();
+        app.window = new Window();
     }
 
     @Test
     void testMaximize() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                // Act
+                app.maximize();
 
+                // Assert
+                assertTrue(App.window.isFullScreen());
+            }
+        });
     }
 
     @Test
     void testMinimize() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                // Act
+                app.minimize();
 
+                // Assert
+                assertTrue(App.window.isIconified());
+            }
+        });
     }
 
     @Test
-    void testStart() {
+    void testClose() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                // Act
+                app.close();
 
+                // Assert
+                assertFalse(App.window.isShowing());
+            }
+        });
     }
 }
